beb1d90864f64693c166ddcaa013bfa0afcceee7
Nando Ellis <nellis@nandohost.nandomain>
2020-10-27 13:34:35 -0400

    first commit - code is generated from start.spring.io, and can generate code into target

---
+++ src/main/java/MadHattr/madHattrinternalproject/MadHattrInternalProjectApplication.java
@@ -0,0 +1,13 @@
+package MadHattr.madHattrinternalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadHattrInternalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadHattrInternalProjectApplication.class, args);
+	}
+
+}
